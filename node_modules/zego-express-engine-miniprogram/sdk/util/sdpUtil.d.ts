105a96fa60898b78ced44e9c6800d5f0d68f9b88
haleycai <haleycai@zego.im>
2020-04-07 18:48:32 +0800

    gitignore

--- node_modules/zego-express-engine-miniprogram/sdk/util/sdpUtil.d.ts
+++
@@ -1,5 +0,0 @@
-import { ZegoVideoCodec } from '../../types';
-export declare class SdpUtil {
-    static zegoSdp(sdp: string): string;
-    static getSDPByVideDecodeType(sdp: string, type: ZegoVideoCodec): string;
-}
--- node_modules/zego-express-engine-miniprogram/sdk/wechatMini/zego.logger.wx.d.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from '../common/zego.logger';
-import { ENUM_LOG_LEVEL } from '../common/zego.entity';
-export declare class LoggerWechat extends Logger {
-    openWebSocketLogServer(url: string): void;
-    SendHttpsLog(): void;
-    logReportParamList(level: ENUM_LOG_LEVEL, logInfo: any): string[];
-}
